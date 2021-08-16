fcaad822846a6c68890894307bced469fb98e4e0
matheusfidelis <matheusfidelis2@gmail.com>
2021-08-16 12:29:31 -0300

    Criação das classes Aluno e Atividade

---
+++ entidade/aluno.py
@@ -0,0 +1,32 @@
+
+
+class Aluno:
+    def __init__(self, nome: str, matricula: str, idade: int):
+        self.__nome = nome
+        self.__nome = matricula
+        self.__nome = idade
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def matricula(self):
+        return self.__matricula
+
+    @matricula.setter
+    def matricula(self, matricula):
+        self.__matricula = matricula
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @idade.setter
+    def idade(self, idade):
+        self.__idade = idade
+
---
+++ entidade/atividades.py
@@ -0,0 +1,58 @@
+
+
+class Atividades:
+    def __init__(self, titulo: str, descricao: str, prazo: date, data_entregue: date, nota: int, status: str):
+        self.__titulo = titulo
+        self.__descricao = descricao
+        self.__prazo = prazo
+        self.__data_entregue = data_entregue
+        self.__nota = nota
+        self.__status = status
+        
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, titulo):
+        self.__titulo = titulo
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao(self, descricao):
+        self.__descricao = descricao
+
+    @property
+    def prazo(self):
+        return self.__prazo
+
+    @prazo.setter
+    def prazo(self, prazo):
+        self.__prazo = prazo
+
+    @property
+    def data_entregue(self):
+        return self.__data_entregue
+
+    @data_entregue.setter
+    def data_entregue(self, data_entregue):
+        self.__data_entregue = data_entregue
+
+    @property
+    def nota(self):
+        return self.__nota
+
+    @data_entregue.setter
+    def nota(self, nota):
+        self.__nota = nota
+
+    @property
+    def status(self):
+        return self.__status
+
+    @data_entregue.setter
+    def status(self, status):
+        self.__status = status
